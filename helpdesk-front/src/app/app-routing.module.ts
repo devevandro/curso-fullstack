0e63b3097d5383c46a8e1d62606c20aa9b2e697e
devevandro <dev.evandro@gmail.com>
2021-09-22 23:05:29 -0300

    cria componente de chamados

--- helpdesk-front/src/app/app-routing.module.ts
+++ helpdesk-front/src/app/app-routing.module.ts
@@ -13,6 +13,7 @@ import { ClienteListComponent } from './components/cliente/cliente-list/cliente-
 import { ClienteCreateComponent } from './components/cliente/cliente-create/cliente-create.component';
 import { ClienteUpdateComponent } from './components/cliente/cliente-update/cliente-update.component';
 import { ClienteDeleteComponent } from './components/cliente/cliente-delete/cliente-delete.component';
+import { ChamadoListComponent } from './components/chamado/chamado-list/chamado-list.component';
 
 const routes: Routes = [
   {
@@ -59,6 +60,10 @@ const routes: Routes = [
       {
         path: 'clientes/delete/:id',
         component: ClienteDeleteComponent,
+      },
+      {
+        path: 'chamados',
+        component: ChamadoListComponent,
       }
     ],
   },
--- helpdesk-front/src/app/app.module.ts
+++ helpdesk-front/src/app/app.module.ts
@@ -35,6 +35,7 @@ import { ClienteDeleteComponent } from './components/cliente/cliente-delete/clie
 import { ClienteCreateComponent } from './components/cliente/cliente-create/cliente-create.component';
 import { ClienteListComponent } from './components/cliente/cliente-list/cliente-list.component';
 import { ClienteUpdateComponent } from './components/cliente/cliente-update/cliente-update.component';
+import { ChamadoListComponent } from './components/chamado/chamado-list/chamado-list.component';
 
 @NgModule({
   declarations: [
@@ -50,7 +51,8 @@ import { ClienteUpdateComponent } from './components/cliente/cliente-update/clie
     ClienteDeleteComponent,
     ClienteCreateComponent,
     ClienteListComponent,
-    ClienteUpdateComponent
+    ClienteUpdateComponent,
+    ChamadoListComponent
   ],
   imports: [
     BrowserModule,
